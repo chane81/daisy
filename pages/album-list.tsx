13b7cc392a9ddf783118c7e52a034b50cefb0cfe
BM <chane81@naver.com>
2019-06-08 06:07:52 +0900

    pages 에 해당파일 추가

---
+++ pages/album-list.tsx
@@ -0,0 +1,15 @@
+import { inject, observer } from 'mobx-react';
+import { Component } from 'react';
+import Master from '../components/layout/Master';
+
+interface IProps {
+	store?: any;
+}
+
+class AlbumList extends Component<IProps> {
+	public render() {
+		return <Master>앨범 리스트</Master>;
+	}
+}
+
+export default inject(({ store }) => ({ store }))(observer(AlbumList));
---
+++ pages/artist-list.tsx
@@ -0,0 +1,15 @@
+import { inject, observer } from 'mobx-react';
+import { Component } from 'react';
+import Master from '../components/layout/Master';
+
+interface IProps {
+	store?: any;
+}
+
+class ArtistList extends Component<IProps> {
+	public render() {
+		return <Master>가수별 리스트</Master>;
+	}
+}
+
+export default inject(({ store }) => ({ store }))(observer(ArtistList));
