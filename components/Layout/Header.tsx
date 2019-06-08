f897ba8d87618d79370ad36dd881ec09b8900ac0
BM <chane81@naver.com>
2019-06-08 16:10:18 +0900

    Header 컴포넌트 마크업

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import '../../styles/Layout/Header.scss';
 
-interface IProps {
-	propsValue?: string;
-}
+const Header = () => {
+	return (
+		<div className='header-wrapper'>
+			<div className='nav-area'>
+				<i className='fas fa-bars fa-lg' />
+				<span>DAISY</span>
+			</div>
+			<div className='search-area'>
+				<input
+					className='search-box'
+					type='text'
+					placeholder='검색할 노래/가수를 입력해 주세요.'
+				/>
+				<div className='search-icon'>
+					<i className='fas fa-search' />
+				</div>
+			</div>
 
-const Header = (props: IProps) => <div>헤더</div>;
+			<hr />
+		</div>
+	);
+};
 
 export default Header;
