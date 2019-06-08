1bb8e1770d4cdaf1d4013b0feffc92d1f4a7c84b
BM <chane81@naver.com>
2019-06-08 10:40:04 +0900

    로딩컴포넌트 추가

--- components/Etc/Loading.tsx
+++ components/Etc/Loading.tsx
@@ -1,5 +1,5 @@
-import classNames from 'components/Etc/node_modules/classnames/bind';
-import React from 'components/Etc/node_modules/react';
+import classNames from 'classnames/bind';
+import React from 'react';
 import styles from '../styles/Loading.scss';
 
 const cx = classNames.bind(styles);
@@ -9,7 +9,6 @@ interface IProps {
 }
 
 const Loading: React.FC<IProps> = (props: IProps) => {
-	console.log('d');
 	return (
 		<div className={cx('Loading', { 'bg-show': props.isBgShow })}>
 			<i className='fas fa-circle-notch fa-spin fa-4x' />
