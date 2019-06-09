da43e1ee03ad8ee4e350591eaaae0cc922e89380
BM <chane81@naver.com>
2019-06-09 14:24:24 +0900

    버튼컴포넌트 추가

---
+++ components/Button/LeftMenuBtn.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+/**
+ * 설명:						레프트 메뉴 버튼 컴포넌트
+ * className:				css class 명
+ * iconClassName: 	fontawesome 의 아이콘 className(ex. fas fa-fire fa-lg)
+ * btnTextName: 		버튼 표시 이름
+ * depth:						뎁스증가에 따라서 오른쪽으로 컨텐츠가 이동함
+ */
+interface IProps {
+	className?: string;
+	iconClassName?: string;
+	btnTextName?: string;
+	depth?: number;
+}
+
+const LeftMenuBtnWrapper = styled('div')<IProps>`
+	display: flex;
+	flex-direction: row;
+	justify-content: start;
+	align-items: center;
+
+	width: 100%;
+	line-height: 3.5em;
+
+	font-family: 'Noto Sans KR', sans-serif;
+
+	:hover {
+		background-color: #e9ecef;
+		cursor: pointer;
+	}
+
+	${(props: IProps) => {
+		const space =
+			props.depth === 2
+				? '2rem'
+				: props.depth === 3
+				? '3rem'
+				: props.depth === 4
+				? '4rem'
+				: '1rem';
+
+		return css`
+			.icon {
+				width: 20%;
+				margin: 0 0 0 ${space};
+			}
+		`;
+	}}
+`;
+const LeftMenuBtn: React.FC<IProps> = props => {
+	return (
+		<LeftMenuBtnWrapper {...props}>
+			<div className='icon'>
+				<i className={props.iconClassName} />
+			</div>
+			<div>{props.btnTextName}</div>
+		</LeftMenuBtnWrapper>
+	);
+};
+
+export default LeftMenuBtn;
--- components/Button/Navbar.tsx
+++ components/Button/Navbar.tsx
@@ -1,44 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/**
+ * 설명:						네비바 버튼
+ * className:				css class 명
+ * btnTextName: 		버튼 표시 이름
+ */
 interface IProps {
 	className?: string;
+	btnTextName?: string;
 }
 
 const NavbarWrapper = styled('div')`
-	margin: 1em;
-	width: 25px;
-	cursor: pointer;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
 
-	:after,
-	:before,
-	div {
-		background-color: black;
-		border-radius: 3px;
-		content: '';
-		display: block;
-		height: 3px;
-		margin: 5px 0;
-		transition: all 0.2s ease-in-out;
+	.nav-bar {
+		margin: 1em;
+		width: 25px;
+		cursor: pointer;
+
+		:after,
+		:before,
+		div {
+			background-color: black;
+			border-radius: 3px;
+			content: '';
+			display: block;
+			height: 3px;
+			margin: 5px 0;
+			transition: all 0.2s ease-in-out;
+		}
+
+		:hover:before {
+			transform: translateY(8px) rotate(130deg);
+		}
+
+		:hover:after {
+			transform: translateY(-8px) rotate(-133deg);
+		}
+
+		:hover div {
+			transform: scale(0);
+		}
 	}
 
-	:hover:before {
-		transform: translateY(8px) rotate(130deg);
-	}
-
-	:hover:after {
-		transform: translateY(-8px) rotate(-133deg);
-	}
-
-	:hover div {
-		transform: scale(0);
+	.nav-text {
+		text-align: left;
+		font-family: 'Impact', sans-serif;
+		font-size: 1.6rem;
+		width: 15%;
+		i {
+			margin: 0 25px 0 15px;
+		}
 	}
 `;
 
 const Navbar: React.FC<IProps> = props => {
 	return (
 		<NavbarWrapper {...props}>
-			<div />
+			<div className='nav-bar'>
+				<div />
+			</div>
+			<div className='nav-text'>
+				<span>{props.btnTextName}</span>
+			</div>
 		</NavbarWrapper>
 	);
 };
