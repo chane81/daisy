892720f4ee73c16774edc193d3663992d92f3f44
BM <chane81@naver.com>
2019-06-19 19:45:56 +0900

    api 루트 스토어 추가

---
+++ stores/apiStore.ts
@@ -0,0 +1,50 @@
+import { flow, Instance, types } from 'mobx-state-tree';
+import { getPlayListItems, getSearch } from '../apis/youtubeApi';
+import apiItemsStore from './apiItemsStore';
+
+const model = types
+	.model('apiModel', {
+		/** 스토어 아이덴티티 */
+		identifier: types.optional(types.identifier, 'apiModel'),
+
+		/** 재생목록 */
+		playlistItems: types.array(apiItemsStore.model)
+	})
+	.actions(self => ({
+		/* 재생목록가져오기
+		 * playlistId: 가져올 플레이리스트 ID
+		 * maxResults: 최대 개수
+		 */
+		getPlayListItems: flow(function*(
+			playlistId: string,
+			maxResults: number
+		) {
+			const res = yield getPlayListItems(playlistId, maxResults);
+
+			console.log('res:', res);
+			self.playlistItems = res;
+		}),
+		getSearch: flow(function*(searchText: string, maxResults: number) {
+			const res = yield getSearch(searchText, maxResults);
+
+			console.log('search res:', res);
+			self.playlistItems = res;
+		})
+	}));
+
+const defaultValue = {
+	identifier: 'apiModel',
+	playItemList: []
+};
+
+const create = model.create(defaultValue);
+
+const apiStore = {
+	create,
+	defaultValue,
+	model
+};
+
+export type IApiModelType = Instance<typeof model>;
+
+export default apiStore;
