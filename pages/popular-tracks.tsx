5d933720b424b3700095dd73d8abd3c5c25ddb32
BM <chane81@naver.com>
2019-06-19 19:44:52 +0900

    인기트랙 추가, 스타일링

--- pages/popular-tracks.tsx
+++ pages/popular-tracks.tsx
@@ -1,61 +1,82 @@
-import { inject, observer } from 'mobx-react';
 import { Component } from 'react';
+import styled, { css } from 'styled-components';
+import { inject, observer } from 'mobx-react';
 import YouTube from 'react-youtube';
 import Master from '../components/Layout/Master';
 import { IStore } from '../stores/storeTypes';
 
+import ThumbnailListCardContainer from '../containers/Cards/ThumbnailListCardContainer';
+
 interface IProps {
 	store?: IStore;
 }
+
+const PopularTracksWrapper = styled('div')`
+	.youtube-wrapper {
+		div {
+			padding: 1rem 2rem 1.5rem 2rem;
+
+			.youtube {
+				width: 100%;
+			}
+		}
+	}
+`;
 
 class PopularTracks extends Component<IProps> {
 	public constructor(props) {
 		super(props);
 
 		// 인기트랙 불러오기
-		this.props.store!.playItemCollectionModel.getPlayList(
+		this.props.store!.apiModel.getPlayListItems(
 			'PLTDluH66q5mpm-Bsq3GlwjMOHITt2bwXE',
 			20
 		);
 	}
 
-	// 이미지 클릭시 해당노래 play
-	public handleClick = e => {
-		const { playerModel } = this.props.store!;
-
-		const videoId = e.currentTarget.dataset.videoId;
-
-		playerModel.setVideoId(videoId);
-	};
-
 	// 플레이어 ready 시에 스토어에 플레이어객체 저장
 	public handleReady = e => {
-		this.props.store!.playerModel.setPlayer(e.target);
+		const { apiModel, playerModel } = this.props.store!;
+
+		// 디폴트 videoId 세팅
+		const videoId = apiModel.playlistItems[0].videoId;
+		playerModel.setVideoId(videoId);
+		playerModel.setPlayer(e.target);
+		playerModel.setStop();
+	};
+
+	// 썸네일 이미지 클릭시 유튜브 Play
+	public handleThumbnailClick = (videoId: string) => {
+		const { playerModel } = this.props.store!;
+
+		playerModel.setVideoId(videoId);
+		playerModel.setPlay();
 	};
 
 	public render() {
-		const { playItemCollectionModel, playerModel } = this.props.store!;
+		const { playerModel } = this.props.store!;
 
 		return (
-			<Master>
-				<div>
-					<YouTube
-						videoId={playerModel.videoId}
-						opts={playerModel.opts}
-						onReady={this.handleReady}
-					/>
-				</div>
-				<div>
-					{playItemCollectionModel.playItemList.map(data => (
-						<img
-							key={data.id}
-							data-video-id={data.resourceId.videoId}
-							src={data.thumbnails.medium!.url}
-							onClick={this.handleClick}
+			<PopularTracksWrapper>
+				<Master>
+					<div className='youtube-wrapper'>
+						<YouTube
+							className='youtube'
+							videoId={playerModel.videoId}
+							opts={playerModel.opts}
+							onReady={this.handleReady}
 						/>
-					))}
-				</div>
-			</Master>
+					</div>
+
+					<div style={{ width: '100%' }}>
+						<hr style={{ width: '50%', border: '0.5px solid #868e96' }} />
+					</div>
+
+					<ThumbnailListCardContainer
+						handleThumbnailClick={this.handleThumbnailClick}
+					/>
+				</Master>
+			</PopularTracksWrapper>
 		);
 	}
 }
