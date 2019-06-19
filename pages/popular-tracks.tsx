22249fa0d449c0cccf11621980e09491eae1f518
BM <chane81@naver.com>
2019-06-20 02:16:52 +0900

    스타일 수정

--- pages/popular-tracks.tsx
+++ pages/popular-tracks.tsx
@@ -6,20 +6,54 @@ import Master from '../components/Layout/Master';
 import { IStore } from '../stores/storeTypes';
 
 import ThumbnailListCardContainer from '../containers/Cards/ThumbnailListCardContainer';
+import { getPlayListItems } from '../apis/youtubeApi';
 
 interface IProps {
 	store?: IStore;
 }
 
 const PopularTracksWrapper = styled('div')`
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: end;
 	.youtube-wrapper {
-		div {
-			padding: 1rem 2rem 1.5rem 2rem;
+		/* 16:9 */
+		min-width: calc(70vw - 11.4rem);
+		max-width: calc(16 * calc(100vh - 115px) / 9);
 
+		div {
+			/* 16:9 */
+			/* width:height ratio = 16/9 = 1.778 */
+			/* height:width ratio = 9/16 = .5625  */
 			.youtube {
+				display: block;
 				width: 100%;
+				height: calc(70vw * 9 / 16);
+				max-height: calc(100vh - 115px - 9.8rem);
 			}
 		}
+		.youtube-title {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border: 1px solid gray;
+			height: 9.8rem;
+			font-family: 'TmonMonsori', sans-serif;
+			font-size: 2rem;
+			font-weight: 100;
+			max-width: calc(16 * calc(100vh - 115px) / 9);
+		}
+
+		@media screen and (max-width: 767px) {
+			.youtube-title {
+				font-size: 1.5rem;
+			}
+		}
+	}
+
+	.thumbnail-list {
+		/* width: 20%; */
+		border: 1px solid green;
 	}
 `;
 
@@ -37,19 +71,21 @@ class PopularTracks extends Component<IProps> {
 	// 플레이어 ready 시에 스토어에 플레이어객체 저장
 	public handleReady = e => {
 		const { apiModel, playerModel } = this.props.store!;
+		const item = apiModel.playlistItems[0];
 
 		// 디폴트 videoId 세팅
-		const videoId = apiModel.playlistItems[0].videoId;
-		playerModel.setVideoId(videoId);
+		playerModel.setVideoId(item.videoId);
 		playerModel.setPlayer(e.target);
+		playerModel.setTitle(item.title);
 		playerModel.setStop();
 	};
 
 	// 썸네일 이미지 클릭시 유튜브 Play
-	public handleThumbnailClick = (videoId: string) => {
+	public handleThumbnailClick = (videoId: string, title: string) => {
 		const { playerModel } = this.props.store!;
 
 		playerModel.setVideoId(videoId);
+		playerModel.setTitle(title);
 		playerModel.setPlay();
 	};
 
@@ -57,8 +93,8 @@ class PopularTracks extends Component<IProps> {
 		const { playerModel } = this.props.store!;
 
 		return (
-			<PopularTracksWrapper>
-				<Master>
+			<Master>
+				<PopularTracksWrapper>
 					<div className='youtube-wrapper'>
 						<YouTube
 							className='youtube'
@@ -66,17 +102,15 @@ class PopularTracks extends Component<IProps> {
 							opts={playerModel.opts}
 							onReady={this.handleReady}
 						/>
-					</div>
-
-					<div style={{ width: '100%' }}>
-						<hr style={{ width: '50%', border: '0.5px solid #868e96' }} />
+						<div className='youtube-title'>{playerModel.title}</div>
 					</div>
 
 					<ThumbnailListCardContainer
+						className='thumbnail-list'
 						handleThumbnailClick={this.handleThumbnailClick}
 					/>
-				</Master>
-			</PopularTracksWrapper>
+				</PopularTracksWrapper>
+			</Master>
 		);
 	}
 }
