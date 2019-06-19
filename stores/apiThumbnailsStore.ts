b1ea64067b12a920aa2bddfdb5bbb05700b426cc
BM <chane81@naver.com>
2019-06-19 19:45:38 +0900

    유튜브api 썸네일 스토어 추가

---
+++ stores/apiThumbnailsStore.ts
@@ -0,0 +1,41 @@
+import { Instance, types } from 'mobx-state-tree';
+
+/** 유튜브 API - 썸네일 사이즈, URL 정보 모델 */
+const apiThumbnailDetailModel = types.model('apiThumbnailDetailModel', {
+	url: types.string,
+	width: types.number,
+	height: types.number
+});
+
+/** 유튜브 API - 썸네일 사이즈별 모델 */
+const model = types.model('apiThumbnailsModel', {
+	/** 스토어 아이덴티티 */
+	identifier: types.optional(types.identifier, 'apiThumbnailsModel'),
+	default: types.maybe(apiThumbnailDetailModel),
+	medium: types.maybe(apiThumbnailDetailModel),
+	high: types.maybe(apiThumbnailDetailModel),
+	standard: types.maybe(apiThumbnailDetailModel),
+	maxres: types.maybe(apiThumbnailDetailModel)
+});
+
+/** 초기화 값 */
+const defaultValue = {
+	identifier: 'apiThumbnailsModel',
+	default: undefined,
+	medium: undefined,
+	high: undefined,
+	standard: undefined,
+	maxres: undefined
+};
+
+const create = model.create(defaultValue);
+
+const apiThumbnailsStore = {
+	create,
+	defaultValue,
+	model
+};
+
+export type IApiThumbnailsModelType = Instance<typeof model>;
+
+export default apiThumbnailsStore;
