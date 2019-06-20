97ad393d0812e3a88873557383030cd512a3005b
BM <chane81@naver.com>
2019-06-21 06:08:54 +0900

    terser, sourcemap, next-compose 플러그인 추가와 설정

--- next.config.js
+++ next.config.js
@@ -5,37 +5,70 @@ const withTypescript = require('@zeit/next-typescript')
 const path = require('path')
 const dotEnv = require('dotenv-webpack')
 const env = require('./config/env')();
+const TerserPlugin = require('terser-webpack-plugin');
+const withSourceMaps = require('@zeit/next-source-maps')
+const withPlugins = require('next-compose-plugins');
 
-module.exports = 
-withTypescript(
-  withSass(
-      withCSS({
-        webpack: function (config) {
-          const originalEntry = config.entry
 
-          config.plugins = config.plugins || []
+module.exports = withPlugins([
+  [withSourceMaps],
+  [withTypescript],
+  [withSass],
+  [withCSS]
+], {
+  // 빌드 디렉토리 설정
+  distDir: 'build',
 
-          config.plugins = [
-            ...config.plugins,
+  // 웹팩 설정
+  webpack: (config, options) => {
+    const originalEntry = config.entry
+    config.plugins = config.plugins || [];
 
-            new webpack.DefinePlugin(env.stringified)
-          ];
+    // 개발모드인지 여부 true/false
+    console.log('is development mod?:', options.dev);
 
-          config.entry = async () => {
-            const entries = await originalEntry()
-
-            if (
-              entries['main.js'] &&
-              !entries['main.js'].includes('./config/polyfills.js')
-            ) {
-              entries['main.js'].unshift('./config/polyfills.js');
-            }
-
-            return entries
-          }
-
-          return config
-        }
+    // 기본 플러그인 어떤것을 로드하는지 확인
+    config.plugins.map(data => {
+      console.log('config name:', data.constructor.name);
     })
-  )
-);
+
+    // 옵션정보 확인
+    console.log('isServer:', options);
+    
+    // 코드 난독화/압축화
+    if (!options.dev && !options.isServer) {
+      config.optimization.minimizer = [new TerserPlugin({
+        parallel: true,
+        sourceMap: true
+      })]
+    }
+
+    // entry 설정
+    config.entry = async () => {
+      const entries = await originalEntry()
+
+      // polyfill 설정
+      if (
+        entries['main.js'] &&
+        !entries['main.js'].includes('./config/polyfills.js')
+      ) {
+        entries['main.js'].unshift('./config/polyfills.js');
+      }
+
+      return entries
+    }
+
+    // plugin 설정
+    config.plugins = [
+      ...config.plugins,
+
+      // ENV 변수 설정
+      new webpack.DefinePlugin({
+        ...env.stringified,
+        'process.env.NODE_ENV': JSON.stringify(options.dev ? 'development' : 'production')
+      })
+    ];
+
+    return config
+  }
+});
