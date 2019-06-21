771949bbd451c0ecc1b292f7f2b481e202464869
BM <chane81@naver.com>
2019-06-21 19:56:30 +0900

    모바일 주소창 닫히는 스크립트 삽입

--- components/Layout/Master.tsx
+++ components/Layout/Master.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import FooterContainer from '../../containers/Layout/FooterContainer';
 import HeaderContainer from '../../containers/Layout/HeaderContainer';
@@ -43,20 +43,33 @@ interface IProps {
 	children?: React.ReactNode;
 }
 
-const Master: React.FC<IProps> = props => (
-	<MasterWrapper {...props}>
-		<div className='body-area'>
-			<LeftMenuContainer className='left-menu-wrapper' />
-			<div className='right-area'>
-				<HeaderContainer />
+const Master: React.FC<IProps> = props => {
+	// 모바일 주소창 닫힘
+	useEffect(() => {
+		window.addEventListener(
+			'load',
+			() => {
+				setTimeout(() => window.scrollTo(0, 1), 100);
+			},
+			false
+		);
+	}, []);
 
-				<div id='contents' className='contents-area'>
-					{props.children}
+	return (
+		<MasterWrapper {...props}>
+			<div className='body-area'>
+				<LeftMenuContainer className='left-menu-wrapper' />
+				<div className='right-area'>
+					<HeaderContainer />
+
+					<div id='contents' className='contents-area'>
+						{props.children}
+					</div>
 				</div>
 			</div>
-		</div>
-		{/* <FooterContainer /> */}
-	</MasterWrapper>
-);
+			{/* <FooterContainer /> */}
+		</MasterWrapper>
+	);
+};
 
 export default Master;
