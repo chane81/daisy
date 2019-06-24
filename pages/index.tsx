906e01c180a0cd8a2fdee36932226f573f41f4b0
BM <chane81@naver.com>
2019-06-25 04:41:28 +0900

    초기화면 인기트렉컴포넌트로 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { inject, observer } from 'mobx-react';
 import Link from 'next/link';
 import { Component } from 'react';
 import Master from '../components/Layout/Master';
+import PopularTracks from './popular-tracks';
 
 interface IProps {
 	store?: any;
@@ -9,25 +10,7 @@ interface IProps {
 
 class Index extends Component<IProps> {
 	public render() {
-		return (
-			<Master>
-				<Link href='/artist-list'>
-					<a style={{ display: 'block' }} className={'artist'}>
-						가수별 리스트 GO
-					</a>
-				</Link>
-				<Link href='/album-list'>
-					<a style={{ display: 'block' }} className={'album'}>
-						앨범 리스트 GO
-					</a>
-				</Link>
-				<Link href='/top-rank-list'>
-					<a style={{ display: 'block' }} className={'rank'}>
-						탑100 리스트 GO
-					</a>
-				</Link>
-			</Master>
-		);
+		return <PopularTracks />;
 	}
 }
 
