c2a8e3fc52a04d5617dddc84dd9ca143835fe0ef
BM <chane81@naver.com>
2019-06-25 21:36:22 +0900

    채널 리스트가져올수 있게 로직 수정

--- apis/youtubeApi.ts
+++ apis/youtubeApi.ts
@@ -32,41 +32,80 @@ export const getPlayListItems = async (
 	 * _.map(items, _.partialRight(_.pick, ['snippet']).snippet) ===
 	 * _.map(items, data => _.pick(data, ['snippet']).snippet)
 	 */
-	return _.map(items, data => ({
+	const result = _.map(items, data => ({
 		..._.pick(data, ['id']),
 		..._.pick(data, ['snippet']).snippet,
 		videoId: _.get(data, 'snippet.resourceId.videoId')
 	}));
+
+	return result;
 };
+
+interface ISearchParams {
+	searchText: string;
+	maxResults: number;
+	order?: string;
+	type?: string;
+	regionCode?: string;
+	videoDefinition?: string;
+}
 
 /**
  * 유튜브 API - 검색 API 호출
  * @param searchText 검색 키워드
  * @param maxResults 최대출력 개수
  */
-export const getSearch = async (
-	searchText: string,
-	maxResults: number
-) => {
+export const getSearch = async ({
+	searchText = '',
+	maxResults = 10,
+	order = 'viewCount',
+	type = 'video',
+	regionCode = 'KR',
+	videoDefinition = 'high'
+}: ISearchParams) => {
 	const url = `${baseUrl}/search`;
+
+	const params = {
+		key: apiKey,
+		maxResults,
+		part: 'snippet',
+		order,
+		q: searchText,
+		regionCode,
+		type,
+		videoDefinition
+	};
+
+	// videoDefinition 은 type이 video 일 때만 파라메터에 들어가야함
+	if (videoDefinition !== 'video') {
+		delete params.videoDefinition;
+	}
 
 	const {
 		data: { items }
-	} = await axios.get(url, {
-		params: {
-			key: apiKey,
-			maxResults,
-			part: 'snippet',
-			order: 'viewCount',
-			q: searchText,
-			type: 'video',
-			videoDefinition: 'high'
-		}
-	});
+	} = await axios.get(url, { params });
 
-	return _.map(items, data => ({
-		id: _.get(data, 'id.videoId'),
-		..._.pick(data, ['snippet']).snippet,
-		videoId: _.get(data, 'id.videoId')
-	}));
+	let result;
+
+	switch (type) {
+		case 'video': {
+			result = _.map(items, data => ({
+				id: _.get(data, 'id.videoId'),
+				..._.pick(data, ['snippet']).snippet,
+				videoId: _.get(data, 'id.videoId')
+			}));
+
+			break;
+		}
+		case 'channel': {
+			result = _.map(items, data => ({
+				id: _.get(data, 'snippet.channelId'),
+				..._.pick(data, ['snippet']).snippet
+			}));
+
+			break;
+		}
+	}
+
+	return result;
 };
