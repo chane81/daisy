78a5f21663fef69858111ef1ccb7b64f441ae4eb
BM <chane81@naver.com>
2019-06-28 08:26:08 +0900

    컴포넌트 분리작업

--- pages/popular-tracks.tsx
+++ pages/popular-tracks.tsx
@@ -22,44 +22,8 @@ class PopularTracks extends Component<IProps> {
 		);
 	}
 
-	// 플레이어 ready 시에 스토어에 플레이어객체 저장
-	public handleReady = e => {
-		const { apiModel, playerModel } = this.props.store!;
-
-		if (apiModel.playlistItems.length > 0) {
-			const { videoId, title } = apiModel.playlistItems[0];
-
-			// 디폴트 videoId 세팅
-			playerModel.setPlayer({
-				player: e.target,
-				videoId,
-				title
-			});
-			playerModel.setStop();
-		}
-	};
-
-	// 썸네일 이미지 클릭시 유튜브 Play
-	public handleThumbnailClick = (videoId: string, title: string) => {
-		const { playerModel } = this.props.store!;
-
-		playerModel.setPlayer({ videoId, title });
-		playerModel.setPlay();
-	};
-
 	public render() {
-		const { playerModel } = this.props.store!;
-		const { uiModel } = this.props.store!;
-
-		return (
-			<PopularTrackSection
-				className='popular-tracks'
-				playerOptions={playerModel}
-				leftMenuVisible={uiModel.leftMenuVisible}
-				handleYoutubeReady={this.handleReady}
-				handleThumbnailClick={this.handleThumbnailClick}
-			></PopularTrackSection>
-		);
+		return <PopularTrackSection />;
 	}
 }
 
