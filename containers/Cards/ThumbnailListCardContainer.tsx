3ba8a617e4d98e11d7637139742e3fb7691859bb
BM <chane81@naver.com>
2019-06-28 08:24:07 +0900

    props 명 변경 playListItems => apiItems 로 변경

--- containers/Cards/ThumbnailListCardContainer.tsx
+++ containers/Cards/ThumbnailListCardContainer.tsx
@@ -26,7 +26,7 @@ class ThumbnailListCardContainer extends Component<IProps> {
 		return (
 			<ThumbnailListCard
 				className={this.props.className}
-				playListItems={this.props.store!.apiModel.playlistItems}
+				apiItems={this.props.store!.apiModel.apiItems}
 				handleThumbnailClick={this.props.handleThumbnailClick}
 				cardFlexBasis={this.props.cardFlexBasis}
 				cardHeight={this.props.cardHeight}
