058637cdd542762417f1093f73dd251c0c71a4f8
BM <chane81@naver.com>
2019-06-28 08:22:45 +0900

    컴포넌트 분리작업 - 트렉리스트 컴포넌트 생성

---
+++ components/TrackList/TrackList.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { observer } from 'mobx-react';
+import { device } from '../../library/styleHelper';
+import ThumbnailListCardContainer from '../../containers/Cards/ThumbnailListCardContainer';
+import YoutubePlayerContainer from '../../containers/Player/YoutubePlayerContainer';
+
+/**
+ * 설명:									플레이 리스트 컴포넌트
+ * className:							css class 명
+ */
+interface IProps {
+	className?: string;
+	handleThumbnailClick: (
+		videoId: string,
+		title: string,
+		channelId: string
+	) => void;
+}
+
+const TrackListWrapper = styled('div')<IProps>`
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: end;
+	height: calc(100vh - 50px);
+
+	.youtube-player {
+		padding-right: 0.7rem;
+	}
+
+	@media ${device.mobile} {
+		flex-flow: column nowrap;
+		.youtube-player {
+			padding-right: 0;
+		}
+	}
+`;
+
+const TrackList: React.FC<IProps> = props => {
+	return (
+		<TrackListWrapper {...props}>
+			<YoutubePlayerContainer></YoutubePlayerContainer>
+			<ThumbnailListCardContainer
+				handleThumbnailClick={props.handleThumbnailClick}
+				cardFlexBasis='13rem'
+			></ThumbnailListCardContainer>
+		</TrackListWrapper>
+	);
+};
+
+export default observer(TrackList);
