cc07f9867b910ae152cdeee587f679a7e1a0fccd
BM <chane81@naver.com>
2019-06-30 20:04:50 +0900

    데이터 바인딩로직 변경

--- pages/popular-tracks.tsx
+++ pages/popular-tracks.tsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { IStore } from '../src/stores/storeTypes';
 import PopularTrackSection from '../src/components/PagesSection/PopularTrackSection';
+import _ from 'lodash';
 
 /**
  * 설명:	인기트랙 page
@@ -16,14 +17,46 @@ class PopularTracks extends Component<IProps> {
 		super(props);
 
 		// 인기트랙 불러오기
-		this.props.store!.apiModel.getPlayListItems(
+		this.setDataBinding();
+	}
+
+	// 인기트랙 불러오기
+	public async setDataBinding() {
+		const { apiModel, playerModel } = this.props.store!;
+		const { tracks } = apiModel.popularTrackList;
+
+		// 인기트랙 불러오기
+		await apiModel.getPlayListItems(
 			process.env.YOUTUBE_API_POPULAR_TRACK_PLAYLIST_ID!,
 			20
 		);
+
+		// 유튜브플레이어 디폴트 세팅
+		if (tracks.length > 0) {
+			await playerModel.setPlayer({
+				videoId: tracks[0].videoId,
+				title: tracks[0].title
+			});
+		}
 	}
 
+	// 썸네일 이미지 클릭시 유튜브 Play
+	public handleThumbnailClick = (videoId: string, title: string) => {
+		const { playerModel } = this.props.store!;
+
+		playerModel.setPlayer({ videoId, title });
+		playerModel.setPlay();
+	};
+
 	public render() {
-		return <PopularTrackSection />;
+		const { popularTrackList } = this.props.store!.apiModel;
+
+		return (
+			<PopularTrackSection
+				apiItems={popularTrackList.tracks}
+				handleThumbnailClick={this.handleThumbnailClick}
+			/>
+		);
 	}
 }
 
