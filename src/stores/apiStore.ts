45493c993485d1006f9fea5979c7b2a5454d83ec
BM <chane81@naver.com>
2019-06-30 20:14:14 +0900

    비동기 콜 부분 try catch 처리 로직 넣음

--- src/stores/apiStore.ts
+++ src/stores/apiStore.ts
@@ -5,6 +5,9 @@ import {
 	getChannelPlaylist
 } from '../apis/youtubeApi';
 import apiItemsStore from './apiItemsStore';
+import apiTrackListStore from './apiTrackListStore';
+import apiChannelStore from './apiChannelStore';
+import statusStore from './statusStore';
 import _ from 'lodash';
 
 const model = types
@@ -12,25 +15,34 @@ const model = types
 		/** 스토어 아이덴티티 */
 		identifier: types.optional(types.identifier, 'apiModel'),
 
-		/** 재생목록 */
-		apiItems: types.array(apiItemsStore.model)
+		/** 인기트렉 리스트 */
+		popularTrackList: apiTrackListStore.model,
+
+		/** 채널 정보 */
+		channelInfo: apiChannelStore.model,
+
+		/** 채널 리스트 */
+		channelList: types.array(apiChannelStore.model),
+
+		/** api call 상태: pending, success, fail */
+		status: types.optional(statusStore.model, statusStore.defaultValue)
 	})
 	.actions(self => ({
-		/**
-		 * 채널의 플레이리스트들 가져오기
-		 * channelId: 채널ID
-		 * maxResults: 최대 개수
-		 */
-		getChannelPlaylist: flow(function*(
-			channelId: string,
-			maxResults: number
-		) {
-			// 명시적 초기화
-			applySnapshot(self, defaultValue);
+		// /**
+		//  * 채널의 플레이리스트들 가져오기
+		//  * channelId: 채널ID
+		//  * maxResults: 최대 개수
+		//  */
+		// getChannelPlaylist: flow(function*(
+		// 	channelId: string,
+		// 	maxResults: number
+		// ) {
+		// 	// 명시적 초기화
+		// 	applySnapshot(self, defaultValue);
 
-			// data get
-			self.apiItems = yield getChannelPlaylist(channelId, maxResults);
-		}),
+		// 	// data get
+		// 	self.apiItems = yield getChannelPlaylist(channelId, maxResults);
+		// }),
 		/**
 		 * 재생목록가져오기
 		 * playlistId: 가져올 플레이리스트 ID
@@ -41,10 +53,20 @@ const model = types
 			maxResults: number
 		) {
 			// 명시적 초기화
-			applySnapshot(self, defaultValue);
+			// applySnapshot(self, defaultValue);
+			self.status.setStatus('pending');
 
-			// data get
-			self.apiItems = yield getPlayListItems(playlistId, maxResults);
+			try {
+				// data get
+				self.popularTrackList.tracks = yield getPlayListItems(
+					playlistId,
+					maxResults
+				);
+
+				self.status.setStatus('success');
+			} catch (err) {
+				self.status.setError(err);
+			}
 		}),
 		/**
 		 * video 검색목록 가져오기
@@ -55,11 +77,21 @@ const model = types
 			searchText: string,
 			maxResults: number
 		) {
-			// 명시적 초기화
-			applySnapshot(self, defaultValue);
+			self.status.setStatus('pending');
 
-			// data get
-			self.apiItems = yield getSearch({ searchText, maxResults });
+			try {
+				// data get
+				self.popularTrackList.tracks = yield getSearch({
+					searchText,
+					maxResults
+				}).catch(err => {
+					self.status.setError(err);
+				});
+
+				self.status.setStatus('success');
+			} catch (err) {
+				self.status.setError(err);
+			}
 		}),
 		/**
 		 * channel 검색목록 가져오기
@@ -70,16 +102,17 @@ const model = types
 			searchText: string,
 			maxResults: number
 		) {
-			// 명시적 초기화
-			applySnapshot(self, defaultValue);
-
-			// data get
-			self.apiItems = yield getSearch({
-				searchText,
-				maxResults,
-				order: 'viewcount',
-				type: 'channel'
-			});
+			try {
+				// data get
+				self.channelList = yield getSearch({
+					searchText,
+					maxResults,
+					order: 'viewcount',
+					type: 'channel'
+				});
+			} catch (err) {
+				self.status.setError(err);
+			}
 		}),
 		/** 초기화 */
 		setInit() {
@@ -88,8 +121,10 @@ const model = types
 	}));
 
 const defaultValue = {
-	identifier: 'apiModel',
-	playItemList: []
+	popularTrackList: apiTrackListStore.defaultValue,
+	channelInfo: apiChannelStore.defaultValue,
+	channelList: [],
+	status: statusStore.defaultValue
 };
 
 const create = model.create(defaultValue);
