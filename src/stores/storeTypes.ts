ef3c93a53a4acb5c2ceab6a114fdb66ebe861e9b
BM <chane81@naver.com>
2019-06-30 20:15:29 +0900

    채널, 트랙리스트, 비동기상태 타입 추가

--- src/stores/storeTypes.ts
+++ src/stores/storeTypes.ts
@@ -3,6 +3,9 @@ import { IApiModelType } from './apiStore';
 import { IApiItemsModelType } from './apiItemsStore';
 import { IApiThumbnailsModelType } from './apiThumbnailsStore';
 import { IPlayerModelType } from './playerStore';
+import { IApiChannelModelType } from './apiChannelStore';
+import { IApiTrackListModelType } from './apiTrackListStore';
+import { IStatuslModelType } from './statusStore';
 import { IUiModelType } from './uiStore';
 
 /** 외부에서 타입참조용으로 쓰이는 인터페이스들 */
@@ -12,5 +15,8 @@ export {
 	IApiItemsModelType,
 	IApiThumbnailsModelType,
 	IPlayerModelType,
+	IApiChannelModelType,
+	IApiTrackListModelType,
+	IStatuslModelType,
 	IStore
 };
