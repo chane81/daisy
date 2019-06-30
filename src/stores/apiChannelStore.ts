2a6058d80f01b7c33b34038129625186d6722ff8
BM <chane81@naver.com>
2019-06-30 20:13:15 +0900

    channel store 추가

---
+++ src/stores/apiChannelStore.ts
@@ -0,0 +1,55 @@
+import { Instance, types, applySnapshot, flow } from 'mobx-state-tree';
+import apiTrackListStore from './apiTrackListStore';
+import apiThumbnailStore from './apiThumbnailsStore';
+import {
+	getChannelSimpleInfo,
+	getChannelPlaylist
+} from '../apis/youtubeApi';
+
+/** 유튜브 API - 채널 정보 모델 - 여러개의 트렉리스트 정보가 들어간다. */
+const model = types
+	.model('apiChannelModel', {
+		/** 스토어 아이덴티티 */
+		identifier: types.optional(types.identifier, 'apiChannelModel'),
+		id: types.optional(types.string, ''),
+		title: types.optional(types.string, ''),
+		description: types.optional(types.string, ''),
+		thumbnails: types.maybe(apiThumbnailStore.model),
+		viewCount: types.optional(types.number, 0),
+		subscriberCount: types.optional(types.number, 0),
+		playList: types.optional(types.array(apiTrackListStore.model), [])
+	})
+	.actions(self => ({
+		setChannel: flow(function*(channelId: string, maxResults: number) {
+			// channel simple data get
+			const channelSimpleData = yield getChannelSimpleInfo(channelId);
+
+			// playList data get
+			const channelPlayList = yield getChannelPlaylist(
+				channelId,
+				maxResults
+			);
+
+			const result = {
+				...channelSimpleData,
+				playList: { ...channelPlayList }
+			};
+
+			applySnapshot(self, result);
+		})
+	}));
+
+/** 초기화 값 */
+const defaultValue = {};
+
+const create = model.create(defaultValue);
+
+const apiChannelStore = {
+	create,
+	defaultValue,
+	model
+};
+
+export type IApiChannelModelType = Instance<typeof model>;
+
+export default apiChannelStore;
