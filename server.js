331400cefe91bf216871aa1fb9006cab57741b56
BM <chane81@naver.com>
2019-06-30 20:02:23 +0900

    express server 로직 변경

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = routes.getRequestHandler(nextApp);
-const express = require('express');
-
 
 nextApp.prepare().then(() => {
-	express().use(nextHandler).listen(port, err => {
+  const server = require('express');
+
+	server().use(nextHandler).listen(port, err => {
 		if (err) throw err
     console.log(`> Ready on port: ${port}`)
   });
