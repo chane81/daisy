58214bbd2cb9cff3a862d39d599dcd9fc09180d6
BM <chane81@naver.com>
2019-07-02 02:45:21 +0900

    유튜브플레이어 기본 플레이 정보 세팅부분 삭제

--- pages/popular-tracks.tsx
+++ pages/popular-tracks.tsx
@@ -1,7 +1,8 @@
 import { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { IStore } from '../src/stores/storeTypes';
-import PopularTrackSection from '../src/components/PagesSection/PopularTrackSection';
+// import PopularTrackSection from '../src/components/PagesSection/PopularTrackSection';
+import PopularTracksLayout from '../src/layout/pages/PopularTracksLayout';
 import _ from 'lodash';
 
 /**
@@ -21,42 +22,18 @@ class PopularTracks extends Component<IProps> {
 	}
 
 	// 인기트랙 불러오기
-	public async setDataBinding() {
-		const { apiModel, playerModel } = this.props.store!;
-		const { tracks } = apiModel.popularTrackList;
+	public setDataBinding() {
+		const { apiModel } = this.props.store!;
 
 		// 인기트랙 불러오기
-		await apiModel.getPlayListItems(
+		apiModel.getPlayListItems(
 			process.env.YOUTUBE_API_POPULAR_TRACK_PLAYLIST_ID!,
-			20
+			2
 		);
-
-		// 유튜브플레이어 디폴트 세팅
-		if (tracks.length > 0) {
-			await playerModel.setPlayer({
-				videoId: tracks[0].videoId,
-				title: tracks[0].title
-			});
-		}
 	}
 
-	// 썸네일 이미지 클릭시 유튜브 Play
-	public handleThumbnailClick = (videoId: string, title: string) => {
-		const { playerModel } = this.props.store!;
-
-		playerModel.setPlayer({ videoId, title });
-		playerModel.setPlay();
-	};
-
 	public render() {
-		const { popularTrackList } = this.props.store!.apiModel;
-
-		return (
-			<PopularTrackSection
-				apiItems={popularTrackList.tracks}
-				handleThumbnailClick={this.handleThumbnailClick}
-			/>
-		);
+		return <PopularTracksLayout />;
 	}
 }
 
