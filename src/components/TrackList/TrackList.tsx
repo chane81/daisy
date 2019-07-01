3a8c08b820a1818fb96fef3ab91f93bb99b304fc
BM <chane81@naver.com>
2019-07-02 02:52:06 +0900

    트렉리스트 로직 추가 및 수정, 스타일 수정진행

--- src/components/TrackList/TrackList.tsx
+++ src/components/TrackList/TrackList.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 import { observer } from 'mobx-react';
+import _ from 'lodash';
 import { device } from '../../library/styleHelper';
 import ThumbnailListCard from '../../components/Cards/ThumbnailListCard';
-import YoutubePlayerContainer from '../../containers/Player/YoutubePlayerContainer';
-import { IApiItemsModelType } from '../../stores/storeTypes';
-import _ from 'lodash';
+import YoutubePlayer from '../../components/Player/YoutubePlayer';
+import {
+	IApiItemsModelType,
+	IPlayerModelType
+} from '../../stores/storeTypes';
 
 /**
- * 설명:									플레이 리스트 컴포넌트
- * className:							css class 명
+ * 설명:										트렉리스트 컴포넌트
+ * @className								css class 명
+ * @apiItems								트랙리스트로 표시할 items
+ * @playerOptions						유튜브 플레이어 옵션
+ * @leftMenuVisible					레프트메뉴 visible true/false
+ * @handlePlayerReady				유튜브 플레이어 onReady 핸들러
+ * @handleThumbnailClick		썸네일 이미지 클릭 핸들러
  */
 interface IProps {
 	className?: string;
 	apiItems?: IApiItemsModelType[];
+	playerOptions: IPlayerModelType;
+	leftMenuVisible?: boolean;
+	handlePlayerReady: (e: any) => void;
 	handleThumbnailClick?: (
 		videoId: string,
 		title: string,
@@ -42,7 +53,12 @@ const TrackListWrapper = styled('div')<IProps>`
 const TrackList: React.FC<IProps> = props => {
 	return (
 		<TrackListWrapper {...props}>
-			<YoutubePlayerContainer></YoutubePlayerContainer>
+			<YoutubePlayer
+				playerOptions={props.playerOptions}
+				handleReady={props.handlePlayerReady}
+				leftMenuVisible={props.leftMenuVisible}
+			></YoutubePlayer>
+
 			<ThumbnailListCard
 				handleThumbnailClick={props.handleThumbnailClick}
 				apiItems={props.apiItems}
