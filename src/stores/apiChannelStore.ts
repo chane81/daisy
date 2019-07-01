c850388822310f05d53ec84446b2ea5df42d553a
BM <chane81@naver.com>
2019-07-02 02:52:44 +0900

    채널정보 set 액션 로직 수정 및 주석제거

--- src/stores/apiChannelStore.ts
+++ src/stores/apiChannelStore.ts
@@ -7,37 +7,38 @@ import {
 } from '../apis/youtubeApi';
 
 /** 유튜브 API - 채널 정보 모델 - 여러개의 트렉리스트 정보가 들어간다. */
-const model = types
-	.model('apiChannelModel', {
-		/** 스토어 아이덴티티 */
-		identifier: types.optional(types.identifier, 'apiChannelModel'),
-		id: types.optional(types.string, ''),
-		title: types.optional(types.string, ''),
-		description: types.optional(types.string, ''),
-		thumbnails: types.maybe(apiThumbnailStore.model),
-		viewCount: types.optional(types.number, 0),
-		subscriberCount: types.optional(types.number, 0),
-		playList: types.optional(types.array(apiTrackListStore.model), [])
-	})
-	.actions(self => ({
-		setChannel: flow(function*(channelId: string, maxResults: number) {
-			// channel simple data get
-			const channelSimpleData = yield getChannelSimpleInfo(channelId);
+const model = types.model('apiChannelModel', {
+	/** 스토어 아이덴티티 */
+	identifier: types.optional(types.identifier, 'apiChannelModel'),
+	channelId: types.optional(types.string, ''),
+	title: types.optional(types.string, ''),
+	description: types.optional(types.string, ''),
+	thumbnails: types.maybe(apiThumbnailStore.model),
+	viewCount: types.optional(types.number, 0),
+	subscriberCount: types.optional(types.number, 0)
+	// playList: types.optional(types.array(apiTrackListStore.model), [])
+});
+// .actions(self => ({
+// 	setChannel: flow(function*(channelId: string, maxResults: number) {
+// 		// channel simple data get
+// 		const channelSimpleData = yield getChannelSimpleInfo(channelId);
 
-			// playList data get
-			const channelPlayList = yield getChannelPlaylist(
-				channelId,
-				maxResults
-			);
+// 		// playList data get
+// 		const channelPlayList = yield getChannelPlaylist(
+// 			channelId,
+// 			maxResults
+// 		);
 
-			const result = {
-				...channelSimpleData,
-				playList: { ...channelPlayList }
-			};
+// 		const result = {
+// 			...channelSimpleData,
+// 			playList: { ...channelPlayList }
+// 		};
 
-			applySnapshot(self, result);
-		})
-	}));
+// 		console.log('chSimple:', result);
+
+// 		self = { ...self, ...channelSimpleData };
+// 	})
+// }));
 
 /** 초기화 값 */
 const defaultValue = {};
