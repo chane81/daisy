4f0d28a175a309727c2e49555cbc02064227603a
BM <chane81@naver.com>
2019-07-29 23:47:01 +0900

    콘솔로그 제거

--- src/stores/apiStore.ts
+++ src/stores/apiStore.ts
@@ -60,6 +60,8 @@ const model = types
 					playList: promiseResult[1]
 				};
 
+				console.log('channelInfo:', self.channelInfo);
+
 				self.status.setStatus('success');
 			} catch (err) {
 				self.status.setError(err);
